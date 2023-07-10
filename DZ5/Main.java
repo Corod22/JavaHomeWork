4ae807aa6597708f09e126808b12cc03106e49a6
Alexander <continenttal2015@yandex.ru>
2023-07-10 19:16:30 +0300

    complete lesson 5

---
+++ DZ5/Main.java
@@ -0,0 +1,40 @@
+package DZ5;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+    /*
+     Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1 человек может иметь несколько телефонов.
+     */
+public class Main {
+    
+    public static void addNumber(String key, int value, Map<String, ArrayList<Integer>> map){
+        if (map.containsKey(key)) {
+            map.get(key).add(value);
+        } else {
+            ArrayList<Integer> list = new ArrayList<>();
+            list.add(value);
+            map.put(key, list);
+        }
+
+    }
+    public static void printBook(Map<String, ArrayList<Integer>> map){
+        for (var item : map.entrySet()) {
+            String phones = "";
+            for(int el: item.getValue()){
+                phones = phones + el + ", ";
+            }
+            System.out.printf("%s: %s \n", item.getKey(), phones);
+        }
+    }
+    public static void main(String[] args) {
+        Map<String, ArrayList<Integer>> bookPhone = new HashMap<>();
+        addNumber("Жуков", 894565123, bookPhone);
+        addNumber("Петров", 891584234, bookPhone);
+        addNumber("Яковлеек", 898446585, bookPhone);
+        addNumber("Измайлов", 898556477, bookPhone);
+        addNumber("Мухич", 892356233, bookPhone);
+        addNumber("Рыбкина", 892787897, bookPhone);
+        printBook(bookPhone);
+       }
+}
